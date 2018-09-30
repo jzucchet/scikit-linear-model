94341fdc76e2879e2473c89b5ea17654d595fb7b
jean <jean@seven4n.com>
2018-09-30 18:26:11 -0300

    Base project

---
+++ README.md
@@ -0,0 +1,32 @@
+# Linear models
+
+Data parsing, scattering and training with basic ML linear algorithms. Adaptations and extensions to 
+Aurélien Geron exercises from [Hands-On Machine Learning with Scikit-Learn and Tensor Flow](https://www.amazon.com/Hands-Machine-Learning-Scikit-Learn-TensorFlow/dp/1491962291) 
+
+## Getting Started
+
+Project available thru Jupyter notebook
+
+### Data sets
+
+* Better Life Index data at [oecd.org](https://stats.oecd.org/index.aspx?DataSetCode=BLI)
+* International Monetary Fund data [imf.org](http://goo.gl/j1MSKe) 
+
+### Built With
+
+* [matpotLib](http://www.matpotlib.org) - The plotting library
+* [numpy](https://www.numpy.org/) - The package for scientific computing
+* [Jupyter](https://jupyter.org/) - The open-source web application for the notebook
+
+### Author
+
+* **Jean Zucchet** - *Initial work* - [GitHub](https://github.com/jzucchet/scikit-linear-model)
+
+### License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+### Acknowledgments
+
+* Aurélien Geron - Handsome Machine Learning [GitHub](https://github.com/ageron/handson-ml)
+
---
+++ main.py
@@ -0,0 +1,70 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sklearn.linear_model
+
+## Load the data
+
+# Better Life Index data
+bli = pd.read_csv("./data/bli_original.csv", delimiter=',')
+life_sat_total = bli[bli["INEQUALITY"] == "TOT"]
+life_sat_total = life_sat_total.pivot(index="Country", columns="Indicator", values="Value")
+# Export the data (if you want to reason on the data before continue)
+life_sat_total.to_csv('./data/life_sat_total.csv')
+
+# International Monetary Fund data
+imf = pd.read_csv("./data/imf-original.csv", thousands='.', delimiter=';', na_values="n/a", index_col="Country")
+imf.rename(columns={"2015": "GDP per capita"}, inplace=True)
+# Export the data (if you want to reason on the data before continue)
+imf.to_csv('./data/gdp_per_capita.csv')
+
+# Prepare the data set
+country_stats = pd.merge(life_sat_total, imf, on='Country')
+country_stats.to_csv('./data/country_stats.csv')
+
+# X as an independent variable
+X = np.c_[country_stats['GDP per capita']]
+
+# y as a dependent variable - predictor
+y = np.c_[country_stats['Life satisfaction']]
+
+## Visualize the data as scatter model to understand the basic correlation between X and y variables
+plt.scatter(X, y)
+plt.xlabel('GDP per Capita')
+plt.ylabel('Life satisfaction')
+plt.show()
+
+# +-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+-+
+# |L|i|n|e|a|r| |R|e|g|r|e|s|s|i|o|n|
+# +-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+-+
+
+# Select a linear model
+model = sklearn.linear_model.LinearRegression()
+## Train the model
+model.fit(X, y)
+#Make a prediction for Cyprus
+X_new = [[22587]] # Cyprus GDP per capita
+print ("Cyprus' Life satisfaction based on a LinearRegression:",model.predict(X_new))
+
+# Visualize the data as bar model to understand the closest country to Cyprus in terms of GDP per capita
+country_stats.filter(items=['GDP per capita']).sort_values("GDP per capita", ascending=False).plot.bar()
+plt.xlabel('Country')
+plt.ylabel('GDP per capita')
+plt.show()
+
+# +-+-+-+-+-+-+-+-+-+-+
+# |K|N|e|i|g|h|b|o|r|s|
+# +-+-+-+-+-+-+-+-+-+-+
+
+#X as an independent variable
+X = np.c_[country_stats['GDP per capita']]
+# y as a dependent variable - predictor
+y = np.c_[country_stats['Life satisfaction']]
+
+# Select a KNeighbor model
+model = sklearn.neighbors.KNeighborsRegressor(n_neighbors=3)
+## Train the model
+model.fit(X, y)
+#Make a prediction for Cyprus
+X_new = [[22587]] # Cyprus GDP per capita
+print ("Cyprus' Life satisfaction based on a KNeighborsRegressor:",model.predict(X_new))
